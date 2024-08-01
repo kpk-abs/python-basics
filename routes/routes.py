24418186112e42df20d7a952dad6b9b6a14c9b32
kpk-abs <kpk@abstractit.in>
2024-08-01 16:30:18 +0530

    Added Route.csv and modified code.

--- routes/routes.py
+++ routes/routes.py
@@ -1,53 +1,21 @@
 import csv
 
-routes = [
-  {
-    "route": 'chennai-trichy',
-    "stops": [
-      { "start": 'chennai', "end": 'viluppuram' },
-      { "start": 'viluppuram', "end": 'trichy' },
-    ],
-  },
-  {
-    "route": 'chennai-karur',
-    "stops": [
-      { "start": 'chennai', "end": 'viluppuram' },
-      { "start": 'viluppuram', "end": 'trichy' },
-      { "start": 'trichy', "end": 'karur' },
-    ]
-  },
-  {
-    "route": 'trichy-tirunelveli',
-    "stops": [
-      { "start": 'trichy', "end": 'madurai' },
-      { "start": 'madurai', "end": 'tirunelveli' },
-    ]
-  },
-  {
-    "route": 'karur-viluppuram',
-    "stops": [
-      { "start": 'karur', "end": 'trichy' },
-      { "start": 'trichy', "end": 'viluppuram' },
-    ]
-  },
-]
-
-def getDistance(stop, distances):
-    for distance in distances:
-            if(stop["start"] == distance["start"] and stop["end"] == distance["end"]) or (stop["start"] == distance["end"] and stop["end"] == distance["start"]):
-                return distance["distance"]
+def getDistance(stops,ind, distances):
+  for distance in distances:
+        if(stops[ind] == distance["Start"] and stops[ind+1] == distance["End"]):
+                return distance["Distance"]
     
 
 def getTotalDistance(route, distances):
     totalDistance = 0
-    for stop in route["stops"]:
-        totalDistance += getDistance(stop, distances)
+    stops = route["Stops"]
+    for ind in range(len(stops)-1):
+      totalDistance += getDistance(stops,ind, distances)
     return totalDistance
     
 def addTotalDistance(route, distances):
     route["totalDistance"] = getTotalDistance(route,distances)
     return route
-
 
 def getComputedDistance(routes, distances):
     routesWithDistance = []
@@ -61,22 +29,39 @@ def convertType(intColumns, distance):
             distance[col] = int(distance[col])
   return distance
 
-def readCSV():
+def covertToArray(arrayColumns, route):
+  for col in arrayColumns:
+          if col in route and route[col]:
+            route[col] = route[col].split(',')
+  return route
+
+def readDistanceCSV():
   distances = []
-  intColumns = ['distance']
-  with open('distances.csv', mode='r') as file:
+  intColumns = ['Distance']
+  with open('Distance.csv', mode='r') as file:
       reader = csv.DictReader(file)
       for distance in reader:
         distance = convertType(intColumns, distance)
         distances.append(distance)
   return distances
 
+def readRoutesCSV():
+  routes = []
+  arrayColumns = ['Stops']
+  with open('Route.csv', mode='r') as file:
+      reader = csv.DictReader(file)
+      for route in reader:
+        route = covertToArray(arrayColumns, route)
+        routes.append(route)
+  return routes  
+
 def displayDistance(routes, distances):
   routesWithTotalDistance = getComputedDistance(routes, distances)
   print(routesWithTotalDistance)
   
 def main():
-  distances = readCSV()
-  displayDistance(routes, distances)
+  distances = readDistanceCSV()
+  routes = readRoutesCSV()
+  displayDistance(routes, distances)  
   
 main()
