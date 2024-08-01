3116400e77530aa6fb48d11c6e8c8e97c1d8b4ef
kpk-abs <kpk@abstractit.in>
2024-08-01 14:45:09 +0530

    Used pandas in routes problem.

---
+++ routesWithPandas/routes.py
@@ -0,0 +1,77 @@
+import pandas as pd
+
+routes = [
+  {
+    "route": 'chennai-trichy',
+    "stops": [
+      { "start": 'chennai', "end": 'viluppuram' },
+      { "start": 'viluppuram', "end": 'trichy' },
+    ],
+  },
+  {
+    "route": 'chennai-karur',
+    "stops": [
+      { "start": 'chennai', "end": 'viluppuram' },
+      { "start": 'viluppuram', "end": 'trichy' },
+      { "start": 'trichy', "end": 'karur' },
+    ]
+  },
+  {
+    "route": 'trichy-tirunelveli',
+    "stops": [
+      { "start": 'trichy', "end": 'madurai' },
+      { "start": 'madurai', "end": 'tirunelveli' },
+    ]
+  },
+  {
+    "route": 'karur-viluppuram',
+    "stops": [
+      { "start": 'karur', "end": 'trichy' },
+      { "start": 'trichy', "end": 'viluppuram' },
+    ]
+  },
+]
+
+def getDistance(stop, distances):
+    for distance in distances:
+            if(stop["start"] == distance["start"] and stop["end"] == distance["end"]) or (stop["start"] == distance["end"] and stop["end"] == distance["start"]):
+                return distance["distance"]
+    
+
+def getTotalDistance(route, distances):
+    totalDistance = 0
+    for stop in route["stops"]:
+        totalDistance += getDistance(stop, distances)
+    return totalDistance
+    
+def addTotalDistance(route, distances):
+    route["totalDistance"] = getTotalDistance(route,distances)
+    return route
+
+
+def getComputedDistance(routes, distances):
+    routesWithDistance = []
+    for route in routes:
+        routesWithDistance.append(addTotalDistance(route, distances))
+    return routesWithDistance
+  
+def convertType(intColumns, distance):
+  for col in intColumns:
+          if col in distance and distance[col]:
+            distance[col] = int(distance[col])
+  return distance
+
+def readCSV():
+  distancesAsArrays = pd.read_csv('distances.csv')
+  distances = distancesAsArrays.to_dict(orient='records')
+  return distances
+
+def displayDistance(routes, distances):
+  routesWithTotalDistance = getComputedDistance(routes, distances)
+  print(routesWithTotalDistance)
+  
+def main():
+  distances = readCSV()
+  displayDistance(routes, distances)
+  
+main()
