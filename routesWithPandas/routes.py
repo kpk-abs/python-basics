b22a7766cf70eb9abd2f374191bdec816fa8a31a
kpk-abs <kpk@abstractit.in>
2024-08-02 10:16:17 +0530

    Added routes CSV for routesWithPandas problem.

--- routesWithPandas/routes.py
+++ routesWithPandas/routes.py
@@ -32,15 +32,17 @@ routes = [
   },
 ]
 
-def getDistance(stop, distances):
-    for distance in distances:
-            if(stop["start"] == distance["start"] and stop["end"] == distance["end"]) or (stop["start"] == distance["end"] and stop["end"] == distance["start"]):
-                return distance["distance"]
+def getDistance(stops,ind, distances):
+  for distance in distances:
+        if(stops[ind] == distance["Start"] and stops[ind+1] == distance["End"]):
+                return distance["Distance"]
+    
 
 def getTotalDistance(route, distances):
     totalDistance = 0
-    for stop in route["stops"]:
-        totalDistance += getDistance(stop, distances)
+    stops = route["Stops"]
+    for ind in range(len(stops)-1):
+      totalDistance += getDistance(stops,ind, distances)
     return totalDistance
     
 def addTotalDistance(route, distances):
@@ -59,17 +61,34 @@ def convertType(intColumns, distance):
             distance[col] = int(distance[col])
   return distance
 
-def readCSV():
-  distancesAsArrays = pd.read_csv('distances.csv')
-  distances = distancesAsArrays.to_dict(orient='records')
+def covertToArray(arrayColumns, route):
+  for col in arrayColumns:
+          if col in route and route[col]:
+            route[col] = route[col].split(',')
+  return route
+
+def readDistancesCSV():
+  distancesAsArray = pd.read_csv('Distance.csv')
+  distances = distancesAsArray.to_dict(orient='records')
   return distances
+
+def readRoutesCSV():
+  routes = []
+  routesAsArray = pd.read_csv('Route.csv')
+  modifiedRoutes = routesAsArray.to_dict(orient='records')
+  arrayColumns = ['Stops']
+  for route in modifiedRoutes:
+    route = covertToArray(arrayColumns, route)
+    routes.append(route)
+  return routes
 
 def displayDistance(routes, distances):
   routesWithTotalDistance = getComputedDistance(routes, distances)
   print(routesWithTotalDistance)
   
 def main():
-  distances = readCSV()
+  distances = readDistancesCSV()
+  routes = readRoutesCSV()
   displayDistance(routes, distances)
   
 main()
